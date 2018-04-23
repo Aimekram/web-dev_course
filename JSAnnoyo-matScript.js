7613c6c9907673b4cacaee85ee316bb51c5ac963
Nina <37555108+Aimekram@users.noreply.github.com>
2018-04-23 23:20:50 +0200

    JS Annoyo-mat

---
+++ JSAnnoyo-matScript.js
@@ -0,0 +1,11 @@
+	var answer = prompt("Are we there yet?");
+	var counter = 1
+
+	while(answer.indexOf("yes")=== -1 && answer !== "yeah") {
+		var answer = prompt("Are we there yet?");
+		counter++;
+	}
+
+	alert("Yay, we finally made it! You achieved level " + counter);
+
+
